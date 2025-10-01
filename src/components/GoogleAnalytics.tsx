2a349e4f10ec11e1c4ee5709df38e16a2f019b29
Jake Dale-Rourke <jakedalerourke@gmail.com>
2025-10-01 10:59:19 +0100

    Feature: Add dual Google Analytics tracking for .com and .co.uk domains

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,16 +1,30 @@
+'use client'
+
 import Script from 'next/script'
+import { useEffect, useState } from 'react'
 
 export function GoogleAnalytics() {
-  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+  const [measurementId, setMeasurementId] = useState<string | null>(null)
 
-  if (!GA_MEASUREMENT_ID) {
+  useEffect(() => {
+    // Determine which GA ID to use based on domain
+    const hostname = window.location.hostname
+    
+    if (hostname.includes('mycvbuddy.co.uk')) {
+      setMeasurementId('G-KDYVEN1G3E') // UK domain
+    } else {
+      setMeasurementId('G-RY8JTS6VFZ') // .com domain (default)
+    }
+  }, [])
+
+  if (!measurementId) {
     return null
   }
 
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -18,7 +32,7 @@ export function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}', {
+          gtag('config', '${measurementId}', {
             page_path: window.location.pathname,
           });
         `}
